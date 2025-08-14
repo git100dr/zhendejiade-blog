a4f483f1d4ef965a1bfc1b1e720c44ca446bc514
git100dr <droping2008@gmail.com>
2025-08-14 23:38:26 +0800

    Update introducing-multi-part-posts-with-nested-routing.mdx

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -25,7 +25,8 @@ export default async function Page() {
               <div key={t} className="mt-2 mr-5 mb-2">
                 <Tag text={t} />
                 <Link
-                  href={`/tags/${slug(t)}`}
+                  //                href={`/tags/${slug(t)}`}
+                  href={`/tags/${encodeURIComponent(t)}`} // **修改这里**
                   className="-ml-2 text-sm font-semibold text-gray-600 uppercase dark:text-gray-300"
                   aria-label={`View posts tagged ${t}`}
                 >
