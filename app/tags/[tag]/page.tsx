623d7cb71737d168db5bd2b6c18d4f490f4b7467
git100dr <droping2008@gmail.com>
2025-08-15 00:47:19 +0800

    Update introducing-multi-part-posts-with-nested-routing.mdx

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -37,9 +37,13 @@ export const generateStaticParams = async () => {
 export default async function TagPage(props: { params: Promise<{ tag: string }> }) {
   const params = await props.params
   const tag = decodeURI(params.tag)
-  const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
+  //   const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
+  const title = /^[a-zA-Z]/.test(tag)
+    ? tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
+    : tag
+
   const filteredPosts = allCoreContent(
-    sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
+    sortPosts(allBlogs.filter((post) => post.tags && post.tags.includes(tag)))
   )
   const totalPages = Math.ceil(filteredPosts.length / POSTS_PER_PAGE)
   const initialDisplayPosts = filteredPosts.slice(0, POSTS_PER_PAGE)
