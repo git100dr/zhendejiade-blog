08ee268be3bb153be4b5b5e227bb2478b01ac3c1
git100dr <droping2008@gmail.com>
2025-08-18 01:05:55 +0800

    combine firebase-based comments and Giscus comment system

--- components/CommentSection.tsx
+++ components/CommentSection.tsx
@@ -183,10 +183,10 @@ const GiscusComponent = () => {
 
     // Replace these attributes with your actual Giscus configuration
     // 请将以下属性替换为您自己的 Giscus 配置
-    script.setAttribute('data-repo', process.env.NEXT_PUBLIC_GISCUS_REPO)
-    script.setAttribute('data-repo-id', process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID)
-    script.setAttribute('data-category', process.env.NEXT_PUBLIC_GISCUS_CATEGORY)
-    script.setAttribute('data-category-id', process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID)
+    script.setAttribute('data-repo', process.env.NEXT_PUBLIC_GISCUS_REPO || '')
+    script.setAttribute('data-repo-id', process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID || '')
+    script.setAttribute('data-category', process.env.NEXT_PUBLIC_GISCUS_CATEGORY || '')
+    script.setAttribute('data-category-id', process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID || '')
     script.setAttribute('data-mapping', 'pathname')
     script.setAttribute('data-reactions-enabled', '1')
     script.setAttribute('data-emit-metadata', '0')
